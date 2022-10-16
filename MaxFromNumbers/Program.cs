fed658781411d7c74d99567358ea45fc21326a97
Klim <naiberd669@bk.ru>
2022-10-17 01:00:09 +0300

    Function 1

--- MaxFromNumbers/Program.cs
+++ MaxFromNumbers/Program.cs
@@ -1,4 +1,13 @@
-﻿int a1 = 15;
+﻿int Max(int arg1, int arg2, int arg3) //Функция с 3-я аргументами.
+{
+    int result = arg1; //Приравниваем Результат к 1-ому аргументу.
+    if (arg2>result) result = arg2; //Сравниваем
+    if (arg3>result) result = arg3; //Сравниваем
+    return result; //Выдаем результат.
+}
+
+
+int a1 = 15;
 int b1 = 21;
 int c1 = 39;
 int a2 = 12;
@@ -8,16 +17,8 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33; // Переменные и их значения, которые у нас есть.
 
-int max = a1; // Определяем переменную значения максиму, в которую положим значение первого аргумента.
-if (b1>max) max =b1; // Если b1 больше максимального, то max присваивается значение b1.
-if (c1>max) max =c1;
-
-if (a2>max) max =a2;
-if (b2>max) max =b2;
-if (c2>max) max =c2;
-
-if (a3>max) max =a3;
-if (b3>max) max =b3;
-if (c3>max) max =c3;
-
+int max1 = Max(a1,b1,c1); // Используем функцию Max(с подставленными соответствующими аргументами). Максимальное значение записываем в переменную max.
+int max2 = Max(a2,b2,c2);
+int max3 = Max(a3,b3,c3);
+int max = Max(max1,max2,max3);
 Console.WriteLine (max);
