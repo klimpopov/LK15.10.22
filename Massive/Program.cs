fdf62c8d02b73d197a7cce070e73d22923373666
Klim <naiberd669@bk.ru>
2022-10-17 01:56:07 +0300

    Function with massive

--- Massive/Program.cs
+++ Massive/Program.cs
@@ -6,5 +6,11 @@
     return result; //Выдаем результат.
 }// Индексы:     0  1  2  3  4  5  6  7  8
 int [] array = {11,21,31,41,15,61,17,18,19}; //Задаём массив
-array[0]=12; 
-Console.WriteLine(array[4]);
+
+//Используем функцию с массивом
+int max = Max(
+    Max(array[0], array[1],array[2]), 
+    Max(array[3], array[4],array[5]),
+    Max(array[6], array[7],array[8])
+);
+Console.WriteLine(max);
