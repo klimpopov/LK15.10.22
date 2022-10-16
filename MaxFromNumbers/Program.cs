48dcaf4b3094481f1b6560d65b22a214e70971be
Klim <naiberd669@bk.ru>
2022-10-17 01:06:16 +0300

    1

--- MaxFromNumbers/Program.cs
+++ MaxFromNumbers/Program.cs
@@ -17,8 +17,13 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33; // Переменные и их значения, которые у нас есть.
 
-int max1 = Max(a1,b1,c1); // Используем функцию Max(с подставленными соответствующими аргументами). Максимальное значение записываем в переменную max.
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1,max2,max3);
+//int max1 = Max(a1,b1,c1); // Используем функцию Max(с подставленными соответствующими аргументами). Максимальное значение записываем в переменную max.
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
+//int max = Max(max1,max2,max3);
+int max = Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
+
 Console.WriteLine (max);
